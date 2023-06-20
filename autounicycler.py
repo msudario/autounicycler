49902e0de9a6c9ac9940e0a4fd3c42dcf87e3338
Mateus <110988842+msudario@users.noreply.github.com>
2023-06-20 10:48:30 -0300

    Update autounicycler.py

--- autounicycler.py
+++ autounicycler.py
@@ -14,6 +14,8 @@ parser.add_argument('-t', '--thread', metavar='', type=int, required=False,
 args = parser.parse_args()
 
 diretorio = os.path.expanduser(f'{args.input}') 
+extensoes = ['.fastq', '.fq']
+
 
 
 def function(diretorio):
@@ -21,28 +23,30 @@ def function(diretorio):
     os.chdir(diretorio)
     if 'results_autounicycler' not in os.listdir():
         os.mkdir('results_autounicycler')
-        
-    for files_to_be_aseembled in os.listdir():
-        if files_to_be_aseembled.endswith('.fastq'):
-            lista.append(files_to_be_aseembled)
+    for arquivos in os.listdir(args.input):
+        for extensao in extensoes:
+            if arquivos.endswith(extensao):   
+                for files_to_be_aseembled in os.listdir():
+                    if files_to_be_aseembled.endswith('.fastq'):
+                        lista.append(files_to_be_aseembled)
             
-    lista.sort()
+            lista.sort()
     
-    lista2 = []
-    for cada_item_da_lista in range(0, len(lista), 2):
+            lista2 = []
+            for cada_item_da_lista in range(0, len(lista), 2):
         
-        lista2 = lista[cada_item_da_lista:cada_item_da_lista+2]
+                lista2 = lista[cada_item_da_lista:cada_item_da_lista+2]
 
-        os.mkdir(os.path.join(f'{diretorio}', 'results_autounicycler', f'{lista2[0]}'))
-        caminho = os.path.join(f'{diretorio}', 'results_autounicycler', f'{lista2[0]}')
+                os.mkdir(os.path.join(f'{diretorio}', 'results_autounicycler', f'{lista2[0]}'))
+                caminho = os.path.join(f'{diretorio}', 'results_autounicycler', f'{lista2[0]}')
         
-        if args.thread:
-            command_line = ['unicycler', '-1', f'{lista2[0]}', '-2', f'{lista2[1]}', '-o', f'{caminho}', '-t', args.thread]
-            subprocess.call(command_line)
+                if args.thread:
+                    command_line = ['unicycler', '-1', f'{lista2[0]}', '-2', f'{lista2[1]}', '-o', f'{caminho}', '-t', args.thread]
+                    subprocess.call(command_line)
             
-        else:
-            command_line = ['unicycler', '-1', f'{lista2[0]}', '-2', f'{lista2[1]}', '-o', f'{caminho}']
-            subprocess.call(command_line)
+                else:
+                    command_line = ['unicycler', '-1', f'{lista2[0]}', '-2', f'{lista2[1]}', '-o', f'{caminho}']
+                    subprocess.call(command_line)
             
 if __name__ == '__main__':
     function(diretorio)
